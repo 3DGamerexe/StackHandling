2e7ab9957d99eaec682c16ceea5937a5ef131307
Aaliyah Madison <81819671+3DGamerexe@users.noreply.github.com>
2024-09-21 14:38:35 -0500

    Documentation

--- StackHandling/StackHandling.cpp
+++ StackHandling/StackHandling.cpp
@@ -1,11 +1,16 @@
-/* Homework #3, Aaliyah Madison*/
-
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <map>
 using namespace std;
 
+/*
+Using my own node class and stack class created using a linkedlist, give the user the ability to input
+a value which can be turned into binary, octal, or hexadecimal. This value will then be pushed into
+the stack.
+*/
+
+//Node class
 class Node {
 
 public:
@@ -23,6 +28,7 @@ public:
     Node* next;
 };
 
+//Creating a linkedlist
 class Node::LinkedList {
 public:
     Node* top = nullptr;
@@ -56,29 +62,31 @@ public:
 
 };
 
+//creating a stack
 class Node::Stack {
 public:
     int current = 0;
     int intValue = 0;
     Node::LinkedList stack;
 
+    //turn values into binary and push to stack
     void binary(string value) {
         intValue = stoi(value);
         int i = 0;
 
-        // MARK: CHANGE 
         while (intValue > 0) {
             stack.push(to_string(intValue % 2));
             intValue = intValue / 2;
             i++;
         }
-        
+
         if (value == "0") {
             stack.push(value);
         }
         stack.print();
     }
 
+    //turn values into octal and push to stack
     void octal(string value) {
         int i = 0;
         intValue = stoi(value);
@@ -95,6 +103,7 @@ public:
         stack.print();
     }
 
+    //turn to hexadecimal and push to the stack
     void hexaDecimal(string value) {
         intValue = stoi(value);
         std::map <int, string> hex = {
@@ -109,15 +118,16 @@ public:
             stack.push(hex.at(remainder));
         }
 
-        while (intValue > 15) {          
-            stack.push(to_string(intValue / 16));    
+        while (intValue > 15) {
+            stack.push(to_string(intValue / 16));
             intValue = intValue / 16;
-        }    
-        
+        }
+
         stack.print();
     }
 };
 
+//menu for user to refer to after every action
 void Menu() {
     std::cout << endl;
     std::cout << std::setw(25) << "M E N U" << endl;
@@ -125,6 +135,7 @@ void Menu() {
     std::cout << "Exit Program(3)" << endl << endl;
 }
 
+//main function
 int main()
 {
     int menuValue;
@@ -133,7 +144,7 @@ int main()
 
     do {
         Menu();
-        std::cout << std::setw(20) << "Choose? "; 
+        std::cout << std::setw(20) << "Choose? ";
         std::cin >> menuValue;
 
         if (menuValue > 3 || menuValue < 0 || cin.fail()) {
@@ -142,26 +153,26 @@ int main()
         }
 
         else {
-            
+
             switch (menuValue) {
-            case 0:
+            case 0:                            //user value turns into binary
                 std::cin >> userValue;
                 stack.binary(userValue);
                 break;
-            case 1:
+            case 1:                            //user value turns into octal
                 std::cin >> userValue;
                 stack.octal(userValue);
                 break;
-            case 2:
+            case 2:                            //user value turns into hexadecimal
                 std::cin >> userValue;
                 stack.hexaDecimal(userValue);
                 break;
             }
         }
 
-        if (menuValue == 3) {
+        if (menuValue == 3) {                  //user exits the program
             std::cout << "Thank you! Have a nice day!" << endl;
         }
-    } while (menuValue != 3);
+    } while (menuValue != 3);                  //unavailable value
 
 }
